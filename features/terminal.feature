f8bd953a430e8e5be01a01717d12d43d6c0d5266
Strand McCutchen <strand@nird.us>
2013-04-12 21:34:53 -0700

    Whitespace fixes

--- features/terminal.feature
+++ features/terminal.feature
@@ -1,15 +1,15 @@
 Feature: Providing passwords at the command line
-	As a person I need to generate memorable passwords at the command line
-	In order to have unique passwords anywhere I use them.
+  As a person I need to generate memorable passwords at the command line
+  In order to have unique passwords anywhere I use them.
 
 @announce
 Scenario: Simple password
-	Given I run `dynamo`
+  Given I run `dynamo`
   Then the output should match /(\w*\s){3}\w*/
 
 @announce
 Scenario: Complex password
-	Given I run `dynamo --complex`
+  Given I run `dynamo --complex`
   Then the output should match /[A-Z]/
   Then the output should match /-/
   Then the output should match /\d/
--- lib/dynamo.rb
+++ lib/dynamo.rb
@@ -1,22 +1,23 @@
 class Dynamo
-	dictionary_path = File.expand_path('../words.txt', __FILE__)
-	WORDS 					||= IO.readlines(dictionary_path).map(&:chomp).uniq!
+  dictionary_path = File.expand_path('../words.txt', __FILE__)
+  WORDS           ||= IO.readlines(dictionary_path).map(&:chomp).uniq!
 
-	def initialize
-	end
+  def initialize
+  end
 
-	def generate_password(style = :simple, words_in_password = 4)
-		word_array = []
+  def generate_password(style = :simple, words_in_password = 4)
 
-		words_in_password.times{ word_array << WORDS.sample }
+  word_array = []
 
-		case style
-		when :simple
-			word_array.join(' ')
-		when :complex
-			digits = %w(0 1 2 3 4 5 6 7 8 9)
-			word_array.first.capitalize!
-			word_array.shuffle.join('-').sub!(/-/, digits.sample)
-		end
-	end
+  words_in_password.times{ word_array << WORDS.sample }
+
+    case style
+    when :simple
+      word_array.join(' ')
+    when :complex
+      digits = %w(0 1 2 3 4 5 6 7 8 9)
+      word_array.first.capitalize!
+      word_array.shuffle.join('-').sub!(/-/, digits.sample)
+    end
+  end
 end
